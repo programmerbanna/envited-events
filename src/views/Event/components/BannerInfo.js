8f18cab7cf2c754e864d2a98ad275178e4c94b63
programmerbanna <programmerbanna@gmail.com>
2022-09-30 19:02:44 +0600

    design completed

--- src/views/Event/components/BannerInfo.js
+++ src/views/Event/components/BannerInfo.js
@@ -5,7 +5,7 @@ import LocationInfo from "../partials/LocationInfo";
 
 const BannerInfo = () => {
   return (
-    <div className="xl:w-[45%] flex flex-col h-full self-start">
+    <div className="xl:w-[45%] flex flex-col h-full self-start xl:p-0 pl-[100px] mt-4">
       {/* header */}
       <HeaderInfo />
       {/* calenda */}
